6a27415105ff588442bbd583abc0f1d3811a5154
Kiran K. <112716069+KeeTl@users.noreply.github.com>
2025-06-06 18:45:33 +0200

    Update ESP_BAT48_1.ino

--- 1_TelemetrieRepriseA0/ESP_BAT48_Homemade/ESP_BAT48_1/ESP_BAT48_1.ino
+++ 1_TelemetrieRepriseA0/ESP_BAT48_Homemade/ESP_BAT48_1/ESP_BAT48_1.ino
@@ -123,7 +123,7 @@ void sendBms24VRequest(uint32_t identifier) {
 // === UART ENVOI VERS SLAVE ===
 void sendBmsDataOverUART() {
   // Exemple d'encodage JSON simple
-  String json = "{ \"Data\": BMS";
+  String json = "{ \"Data\": \"BMS\"";
   json += "\"V\":" + String(bmsData24V.totalVoltage, 1) + ",";
   json += "\"I\":" + String(bmsData24V.current, 1) + ",";
   json += "\"SOC\":" + String(bmsData24V.soc, 1) + ",";
@@ -136,9 +136,9 @@ void sendBmsDataOverUART() {
 
 void sendTemperaturesOverUART() {
     uint16_t* temperatures = multiplexeur1.getTemperatures();    
-    String json = "{\"Data\": Temps";
+    String json = "{\"Data\":\"Temperatures\", \"ThermistorNumber\":" + String(multiplexeur1.numThermistors) + ",";
     for (int i = 0; i < multiplexeur1.numThermistors; i++) {
-        json += "\"Temp" + String(i) + "\":" + String(temperatures[i])
+        json += String(i) + ":" + String(temperatures[i])
         if (i != multiplexeur.numThermistors - 1) json += ",";
     }
     json += "}\n";
